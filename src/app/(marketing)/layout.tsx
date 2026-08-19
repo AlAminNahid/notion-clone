0fa6e34833dc401b9319e9a4e161ffecccc9ce82
AlAminNahid <robin.nahid123@gmail.com>
2026-08-19 19:42:25 +0600

    Fix the background issue of the landing page

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/features/marketing/navbar";
 
 export default function MarketingLayout({ children }: LayoutProps<"/">) {
   return (
-    <div className="h-full dark:bg-[#1F1F1F]">
+    <div className="h-full">
       <Navbar />
       <main className="h-full pt-40">{children}</main>
     </div>
